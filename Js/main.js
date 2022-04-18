2f21271e2f62bc573ee1f62b1cea94d65e6ac21c
kevinGalvis <kevin.davidtt@gmail.com>
2022-04-18 16:08:57 -0500

    agregamos modal contact

--- Js/main.js
+++ Js/main.js
@@ -1,8 +1,26 @@
 // VARIABLES MENU LATERAL 
 const $btnBar = document.querySelector(".bar-btn");
 const $batItems = document.querySelector(".bar");
-
+//VARIABLES MODAL CONTACTAME
+const $ventanModal = document.querySelector(".section-modal");
+const $linkContact = document.querySelector(".contact");
+const $cerrarModal = document.querySelector(".Cerrar");
+const $cancelar = document.querySelector(".cancelar");
 //FUNCIONALIDAD MENU
 $btnBar.addEventListener("click",()=>{
     $batItems.classList.toggle("bar-items_visible");
 });
+
+//FUNCIONALIDAD MODAL CONTACTAME
+$linkContact.addEventListener("click",(e)=>{
+    e.preventDefault();
+  $ventanModal.classList.add("modal-visible");
+})
+$cerrarModal.addEventListener("click",(e)=>{
+    e.preventDefault();
+  $ventanModal.classList.remove("modal-visible");
+})
+$cancelar.addEventListener("click",(e)=>{
+    e.preventDefault();
+  $ventanModal.classList.remove("modal-visible");
+})
