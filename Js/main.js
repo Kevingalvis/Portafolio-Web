fbbebb1cd18d83047402d702b3ffbd0f0174bb54
kevinGalvis <kevin.davidtt@gmail.com>
2022-04-19 17:38:15 -0500

    loader

--- Js/main.js
+++ Js/main.js
@@ -6,11 +6,25 @@ const $ventanModal = document.querySelector(".section-modal");
 const $linkContact = document.querySelector(".contact");
 const $cerrarModal = document.querySelector(".Cerrar");
 const $cancelar = document.querySelector(".cancelar");
+// VARIABLES PRELOADER
+const $loader = document.querySelector(".section-loader");
+
+
+
+
+
+
+
+
+/** ------------------------------------------------- */
+/** ------------------------------------------------- */
 //FUNCIONALIDAD MENU
 $btnBar.addEventListener("click",()=>{
     $batItems.classList.toggle("bar-items_visible");
 });
 
+/** ------------------------------------------------- */
+/** ------------------------------------------------- */
 //FUNCIONALIDAD MODAL CONTACTAME
 $linkContact.addEventListener("click",(e)=>{
     e.preventDefault();
@@ -23,4 +37,15 @@ $cerrarModal.addEventListener("click",(e)=>{
 $cancelar.addEventListener("click",(e)=>{
     e.preventDefault();
   $ventanModal.classList.remove("modal-visible");
+})
+
+/** ------------------------------------------------- */
+/** ------------------------------------------------- */
+//FUNCIONALIDAD PRELOADER
+window.addEventListener("load", ()=>{
+  console.log("asdadasd")
+  setTimeout(function(){
+    $loader.classList.toggle("loader-close");
+  },5000);
+ 
 })
